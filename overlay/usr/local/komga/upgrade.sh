21e889db40ccc68c45f2a7d9220fe1a6e0f9bf32
fuji44 <fuji.44mt@gmail.com>
2021-05-06 05:07:36 +0900

    add komga upgrade script

---
+++ overlay/usr/local/komga/upgrade.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+service komga stop
+
+# Install komga
+: ${komga_app_dir="/usr/local/komga"}
+cd $komga_app_dir
+mv -f release-latest.json release-old.json
+fetch -qo release-latest.json https://api.github.com/repos/gotson/komga/releases/latest
+jq '.assets[] | select(.name | test("komga.*\\.jar")) | .browser_download_url' release-latest.json | xargs fetch -q
+jq '.assets[] | select(.name | test("komga.*\\.jar")) | .name' release-latest.json | xargs -I @ ln -sf @ komga.jar
+
+chown -R komga $komga_app_dir
+
+service komga start
