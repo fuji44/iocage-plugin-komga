e7c4a32709bce50b489f9825f7b74437d23005e0
fuji44 <fuji.44mt@gmail.com>
2021-05-10 09:31:25 +0900

    add post_update.sh

--- post_update.sh
+++ post_update.sh
@@ -3,16 +3,15 @@
 # Install komga
 : ${komga_app_dir="/usr/local/komga"}
 cd $komga_app_dir
+mv -f release-latest.json release-old.json
 fetch -qo release-latest.json https://api.github.com/repos/gotson/komga/releases/latest
 jq '.assets[] | select(.name | test("komga.*\\.jar")) | .browser_download_url' release-latest.json | xargs fetch -q
-jq '.assets[] | select(.name | test("komga.*\\.jar")) | .name' release-latest.json | xargs -I @ ln -s @ komga.jar
+jq '.assets[] | select(.name | test("komga.*\\.jar")) | .name' release-latest.json | xargs -I @ ln -sf @ komga.jar
 
-pw useradd komga -s /bin/csh -m
 chown -R komga $komga_app_dir
-sysrc -f /etc/rc.conf komga_enable="YES"
 
-service komga start
+service komga restart
 
-echo "✅ komga installation is complete!" > /root/PLUGIN_INFO
+echo "✅ komga update is complete!" > /root/PLUGIN_INFO
 echo "App dir: $komga_app_dir" >> /root/PLUGIN_INFO
 echo "Komga version: `jq '.name' release-latest.json`" >> /root/PLUGIN_INFO
