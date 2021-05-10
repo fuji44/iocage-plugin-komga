9c9c4e15c5b079d358259bf8a4a52904c6c8361f
fuji44 <fuji.44mt@gmail.com>
2021-05-10 09:47:31 +0900

    Change to start instead of restart

--- post_update.sh
+++ post_update.sh
@@ -3,14 +3,14 @@
 # Install komga
 : ${komga_app_dir="/usr/local/komga"}
 cd $komga_app_dir
-mv -f release-latest.json release-old.json
+mv -f release-latest.json release-previous.json
 fetch -qo release-latest.json https://api.github.com/repos/gotson/komga/releases/latest
 jq '.assets[] | select(.name | test("komga.*\\.jar")) | .browser_download_url' release-latest.json | xargs fetch -q
 jq '.assets[] | select(.name | test("komga.*\\.jar")) | .name' release-latest.json | xargs -I @ ln -sf @ komga.jar
 
 chown -R komga $komga_app_dir
 
-service komga restart
+service komga start
 
 echo "✅ komga update is complete!" > /root/PLUGIN_INFO
 echo "App dir: $komga_app_dir" >> /root/PLUGIN_INFO
