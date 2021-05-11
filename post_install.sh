9e857d7d64deacb2814d94bf8dd21fae807b9ff5
fuji44 <fuji.44mt@gmail.com>
2021-05-11 10:08:44 +0900

    refactor and miner fix

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Install komga
-: ${komga_app_dir="/usr/local/komga"}
-cd $komga_app_dir
+: "${komga_app_dir='/usr/local/komga'}"
+cd $komga_app_dir || { echo "Komga app dir not found."; exit 1; }
 fetch -qo release-latest.json https://api.github.com/repos/gotson/komga/releases/latest
 jq '.assets[] | select(.name | test("komga.*\\.jar")) | .browser_download_url' release-latest.json | xargs fetch -q
 jq '.assets[] | select(.name | test("komga.*\\.jar")) | .name' release-latest.json | xargs -I @ ln -s @ komga.jar
@@ -15,4 +15,4 @@ service komga start
 
 echo "✅ komga installation is complete!" > /root/PLUGIN_INFO
 echo "App dir: $komga_app_dir" >> /root/PLUGIN_INFO
-echo "Komga version: `jq '.name' release-latest.json`" >> /root/PLUGIN_INFO
+echo "Komga version: $(jq -r '.name' release-latest.json)" >> /root/PLUGIN_INFO
